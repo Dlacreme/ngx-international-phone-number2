80aa3608e4aa40e2c99386cab103309e7c6c3e60
Mathieu Delacroix <mathieu.delacroix.fr@gmail.com>
2020-04-14 09:59:11 +0200

    remove dist

--- dist/phone-number.module.d.ts
+++
@@ -1,4 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export declare class InternationalPhoneNumber2Module {
-    static forRoot(): ModuleWithProviders;
-}
--- dist/phone.directive.d.ts
+++
@@ -1,3 +0,0 @@
-export declare class PhoneMaskCursorProcessorDirective {
-    keypress(event: MouseEvent, target: HTMLInputElement): void;
-}
